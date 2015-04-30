d5d1a869fdb1cf0f5a51858a84a56153787b2963
lupti-udacity <lupti.cheung.udacity@gmail.com>
2015-04-30 09:51:01 -0700

    Document AVAudioEngine workflow, refactor the code, abstract code segment for reuse, add more comments to aid readability.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -6,11 +6,19 @@
 //  Copyright (c) 2015 Lupti. All rights reserved.
 //
 
+/* AVAudioEngine Workflow
+1. Create an engine
+2. Create nodes
+3. Attach nodes to the engine
+4. Connect the nodes together 
+5. Start the engine
+*/
+
 import UIKit
 import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-    
+    //Declare global variables
     var audioPlayer: AVAudioPlayer!
     var receivedAudio: RecordedAudio!
     var audioEngine: AVAudioEngine!
@@ -18,7 +26,7 @@ class PlaySoundsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //Instantiate audioPlayer, audioEngine and audioFile
         audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
         audioPlayer.enableRate = true
         audioEngine = AVAudioEngine()
@@ -31,14 +39,20 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Common function to stop player and engine, set the rate from pass-in parameter, and restart audioPlayer
     func audioPlayerPlayRate(myRate: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
+        stopAudioPlayerAndEngine()
         audioPlayer.rate = myRate
         audioPlayer.currentTime = 0
         audioPlayer.play()
     }
     
+    //Common function to stop player and engine
+    func stopAudioPlayerAndEngine() {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+    }
     
     @IBAction func playSlowAudio(sender: UIButton) {
         audioPlayerPlayRate(0.5)
@@ -61,44 +75,49 @@ class PlaySoundsViewController: UIViewController {
     }
     
     func playAudioWithVariablePitch(pitch: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        //Stop any player still in process
+        stopAudioPlayerAndEngine()
         
+        //Instantiate player and attach it to the audio engine
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
         
+        //Add pitch rate from parameter to the audio engine by attaching it
         var changePitchEffect = AVAudioUnitTimePitch()
         changePitchEffect.pitch = pitch
         audioEngine.attachNode(changePitchEffect)
         
+        //Connect the node and effect to the audio engine
         audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
         audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
         
+        //Attach audio file to the audio engine and start the engine
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         audioEngine.startAndReturnError(nil)
         
+        //Play the audio player
         audioPlayerNode.play()
         
     }
-    
+    //Play recorded audio in rever/echo mode
     func playAudioWithReverb() {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
+        stopAudioPlayerAndEngine()
         
+        //First declare the necessary nodes
         var audioPlayerNode = AVAudioPlayerNode()
         var audioPlayerReverbNode = AVAudioUnitReverb()
         var audioPlayerDistortNode = AVAudioUnitDistortion()
         var audioPlayerDelayNode = AVAudioUnitDelay()
         var audioPlayerMixerNode = AVAudioMixerNode()
         
+        //Attach nodes to the audioEngine
         audioEngine.attachNode(audioPlayerNode)
         audioEngine.attachNode(audioPlayerReverbNode)
         audioEngine.attachNode(audioPlayerDistortNode)
         audioEngine.attachNode(audioPlayerDelayNode)
         audioEngine.attachNode(audioPlayerMixerNode)
         
+        //Connect audioEngine to nodes in series
         audioEngine.connect(audioPlayerNode, to: audioPlayerReverbNode, format: nil)
         audioEngine.connect(audioPlayerReverbNode, to: audioPlayerDistortNode, format: nil)
         audioEngine.connect(audioPlayerDistortNode, to: audioPlayerDelayNode, format: nil)
@@ -106,17 +125,16 @@ class PlaySoundsViewController: UIViewController {
         audioEngine.connect(audioPlayerMixerNode, to: audioEngine.outputNode, format: nil)
         audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
         
+        //Start the audioEngine in reverb mode
         audioEngine.startAndReturnError(nil)
         
+        //Play in reverb mode
         audioPlayerNode.play()
         
     }
     
     
     @IBAction func stopAudioPlayer(sender: UIButton) {
-        audioEngine.stop()
-        audioPlayer.stop()
+        stopAudioPlayerAndEngine()
     }
-    
-
 }
